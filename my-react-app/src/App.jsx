bd91c46513de159b5bd325ad1ebb7318a1b5b2cd
Jonathan Luembe <jonathanluembe@yahoo.com>
2023-10-06 21:51:42 +0200

    Search

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,33 +2,61 @@ import { Input } from "./components/forms/Input.jsx";
 import { Checkbox } from "./components/forms/Checkbox.jsx";
 import { ProductCategoryRow } from "./components/product/ProductCategoryRow.jsx";
 import { ProductRow } from "./components/product/ProductRow.jsx";
+import { useState } from "react";
 
-const Product = [
+const PRODUCTS = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
   { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$1", stocked: false, name: "Passionfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
 ]
 
 function App() {
+
+  const [showStockedOnly, setShowStockedOnly] = useState(false)
+  const [search, setSearch] = useState('')
+
+  const visibleProducts = PRODUCTS.filter(product => {
+    if (showStockedOnly && !product.stocked) {
+      return false
+    }
+
+    if (search && !product.name.includes(search)) {
+      return false
+    }
+    return true
+  })
+
   return <div className="container my-3">
-    <SearchBar />
-    <ProductTable products={Product} />
+    <SearchBar
+      showStockedOnly={showStockedOnly}
+      onStockedOnlyChange={setShowStockedOnly}
+      search={search}
+
+      onSearchChange={setSearch}
+    />
+    <ProductTable products={visibleProducts} />
   </div>
 }
 
-function SearchBar() {
+function SearchBar({ showStockedOnly, onStockedOnlyChange, search, onSearchChange }) {
   return <div>
     <div className="mb-3">
-      <Input value="" onChange={() => null} placeholder="Rechercher..." />
-      <Checkbox id="stocked" checked={false} onChange={() => null} label="N'afficher que les produits en stock" />
+      <Input value={search}
+        onChange={onSearchChange}
+        placeholder="Rechercher..." />
+      <Checkbox
+        id="stocked"
+        checked={showStockedOnly}
+        onChange={(onStockedOnlyChange)}
+        label="N'afficher que les produits en stock" />
     </div>
   </div>
 }
 
-function ProductTable ({ products }) {
+function ProductTable({ products }) {
   const rows = []
   let lastCategory = null
 
